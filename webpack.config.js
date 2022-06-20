f122e981b5c913be464b6ba0d6ce5516ee637b76
felix1251 <domhack123@gmail.com>
2022-06-20 21:24:14 +0800

    ready

--- webpack.config.js
+++ webpack.config.js
@@ -76,9 +76,9 @@ module.exports = {
                   "crypto": require.resolve("crypto-browserify"),
                   "stream": require.resolve("stream-browserify"),
                   "assert": require.resolve("assert"),
-                  "http": require.resolve("stream-http"),
+                  "http": false,
                   "https": require.resolve("https-browserify"),
-                  "os": require.resolve("os-browserify"),
+                  "os": require.resolve("os-browserify/browser"),
                   "url": require.resolve("url")
             }
       },
